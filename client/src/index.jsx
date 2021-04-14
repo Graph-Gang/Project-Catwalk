5c799139cc09c3c3002e69aede45834eea386d3d
Tom <tomshibley@gmail.com>
2021-04-13 17:03:09 -0700

    Test of app load

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const App = () => <></>;
+const App = () => <>My name is Tom</>;
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
