4aace24284364716cea7d9975ea0113cac821bca
Jheuton7 <jeffreyheuton1@gmail.com>
2021-04-13 18:00:24 -0600

    test commit

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -29865,7 +29865,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 var App = function App() {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null);
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, "Hello");
 };
 
 react_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(App, null), document.getElementById('app'));
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const App = () => <></>;
+const App = () => <div>Hello</div>;
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
