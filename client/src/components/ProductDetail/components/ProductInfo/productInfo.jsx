86e4f5c1cc9ebaec9e0c74b71f45566f38d671e9
Daniel Tseng <dnltseng@gmail.com>
2021-04-16 16:10:58 -0700

    Product Info Price + Stars

--- client/src/components/ProductDetail/components/ProductInfo/productInfo.jsx
+++ client/src/components/ProductDetail/components/ProductInfo/productInfo.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import StarRating from './components/starRating.jsx';
 
 const ProductInfo = (props) => (
   <div>
+    <div>
+      <StarRating />
+      <div className='readReviews'>
+        <a href=''>
+          Read all reviews
+        </a>
+      </div>
+    </div>
     <div className='product_category'>
       {props.product.category}
     </div>
     <div className='product_name'>
       {props.product.name}
     </div>
+    <br></br>
+    <div className='product_price'>
+      ${props.product.default_price}
+    </div>
+    <br></br>
   </div>
 )
 
