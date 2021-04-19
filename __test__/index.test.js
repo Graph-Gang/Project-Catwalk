4d63fa8a0299a90339e555e3fc1cf2b6418a95bc
Daniel Tseng <dnltseng@gmail.com>
2021-04-19 10:27:50 -0700

    First Attempts at Writing Tests

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,1 +1,19 @@
-console.log('tests passed');
+import React from 'react';
+import Renderer from 'react-test-renderer';
+
+// const App = require('../client/src/index.jsx');
+const axios = require('axios');
+
+it('returns 200 status code', () => {
+  axios.get('/products')
+    .then((results) => {
+      expect(results.status).toEqual(200);
+    })
+    .catch((error) => {
+      console.log('Failed Test');
+    })
+  })
+
+  // console.log('200');
+  // const getRequest = await App.fetchAll();
+  // expect(getRequest.status).toEqual('200');
