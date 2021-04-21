df88d85d761e07fed6a3883d8e6d1fc4fd1dddb6
Tom <tomshibley@gmail.com>
2021-04-21 12:06:07 -0700

    Added luxon to dependencies and rendered date on review

--- client/src/components/RatingReview/components/reviews/review.jsx
+++ client/src/components/RatingReview/components/reviews/review.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-// import StarRating from '../ProductDetail/components/ProductInfo/components/starRating.jsx';
-// /Users/tomshibley/Project-Catwalk/client/src/components/ProductDetail/components/ProductInfo/components/starRating.jsx
-const Review = (props) => (
-  <li>
-    <props.StarRating rating={props.review.rating}/>
-    <div>{props.review.reviewer_name}</div>
-    <h2>{props.review.summary}</h2>
-    <div>{props.review.body}</div>
-    <button>Helpful?</button>
-    <div>Yes ({props.review.helpfulness})</div>
+import { DateTime } from 'luxon';
 
-  </li>
-)
+
+const Review = (props) => {
+
+  const formatted = DateTime
+  .fromISO(props.review.date)
+  .toLocaleString(DateTime.DATETIME_MED)
+// {DateTime.fromIso(props.review.date)}
+
+  return(
+    <li>
+      <props.StarRating rating={props.review.rating}/>
+      <div>{props.review.reviewer_name}</div>
+      <div>{formatted}</div>
+      <h2>{props.review.summary}</h2>
+      <div>{props.review.body}</div>
+      <button>Helpful?</button>
+      <div>Yes ({props.review.helpfulness})</div>
+
+    </li>
+  )
+}
 
 export default Review;
