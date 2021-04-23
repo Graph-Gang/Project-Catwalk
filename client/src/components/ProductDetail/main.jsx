2822856e4bff69e46f806463c110496c59610270
Daniel Tseng <dnltseng@gmail.com>
2021-04-22 18:51:04 -0700

    Default View of Image Gallery Functionality Complete

--- client/src/components/ProductDetail/main.jsx
+++ client/src/components/ProductDetail/main.jsx
@@ -7,34 +7,58 @@ import Description from './components/ProductInfo/components/description.jsx';
 import Features from './components/ProductInfo/components/features.jsx';
 import Share from './components/ProductInfo/components/sharing.jsx';
 
-const ProductDetail = (props) => (
-  <div>
-    <div className='pd_grid'>
-      <div className='pd_item1'>
-        {/* LeftSideOfMainImage */}
-      </div>
-      <div className='pd_item2'>
-        {/* MainImage */}
-        <ImgGallery product={props.product}/>
-      </div>
-      <div className='pd_item3'>
-        <ProductInfo product={props.product}/>
-        <StyleSelector product_styles={props.product_styles}/>
-        {/* <AddCart product={props.product}/> */}
-      </div>
-      <div className='pd_item4'>
-        <div className='social_media'>
-          <Share />
+class ProductDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: {},
+    }
+    this.handleStyleSelect = this.handleStyleSelect.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.product_styles !== prevProps.product_styles) {
+      this.setState({
+        selected: this.props.product_styles[0],
+      })
+    }
+  }
+
+  handleStyleSelect(selected) {
+    this.setState({
+      selected: selected,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='pd_grid'>
+          <div className='pd_item1'>
+            <ImgGallery selected={this.state.selected}/>
+          </div>
+          <div className='pd_item3'>
+            <ProductInfo product={this.props.product}/>
+            <StyleSelector product_styles={this.props.product_styles} selectStyle={this.handleStyleSelect}/>
+            <div>
+              <AddCart selected={this.state.selected}/>
+            </div>
+          </div>
+          <div className='pd_item4'>
+            <div className='social_media'>
+              <Share />
+            </div>
+          </div>
+          <div className='pd_item5'>
+            <Description product={this.props.product}/>
+          </div>
+          <div className='pd_item6'>
+            <Features product={this.props.product}/>
+          </div>
         </div>
       </div>
-      <div className='pd_item5'>
-        <Description product={props.product}/>
-      </div>
-      <div className='pd_item6'>
-        <Features product={props.product}/>
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default ProductDetail;
